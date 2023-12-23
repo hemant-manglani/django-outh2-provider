5f12c854bff1ebbdd60a3d13cf71d61de1a43a5c
= <=>
2023-12-23 15:40:23 +0530

    change taking value logic

--- users/views.py
+++ users/views.py
@@ -15,44 +15,40 @@ from django.utils.translation import gettext_lazy as _
 from django.db import transaction
 from django.conf import settings
 
+
 class Login(APIView):
     permission_classes = (permissions.AllowAny,)
-    def post(self, request, *args, **kwargs):
-        email = request.POST['email']
-        password = request.POST['password']
-        user_details = models.User.objects.filter(email=email)
-        if user_details:
-            url = request.build_absolute_uri('/o/token/')
-            print("url ::", url)
-            r = requests.post(url, data={'grant_type':'password', # your defined grant type
-                                    'client_id': settings.OAUTH_CLIENT_ID, # your clinet id
-                                    'client_secret': settings.OAUTH_CLIENT_SECRET, #your client secret
-                                    'username': user_details[0].username, # your username that you get from user
-                                    'password':password, #your password that you get from user
-                                    'redirect_uri': request.build_absolute_uri('/menu')
-                            }
-            )
-            print(r.status_code)
-            try:
-                res = r.json
-            except:
-                res = r
-            result = {
-                "status": str(r.status_code),
-                "data": res
-            }
-            print("r::", result)
-            return Response(json.loads(json.dumps(result)))
-        else:
-            return Response(json.loads('{"message": "User does not exists."}'))
 
-    
+    def post(self, request, *args, **kwargs):
+        request_data = request.data
+        try:
+            email = request_data.get('email')
+            password = request_data.get('password')
+            user_details = models.User.objects.filter(email=email)
+            if user_details:
+                url = request.build_absolute_uri('/o/token/')
+                r = requests.post(url, data={'grant_type': 'password',  # your defined grant type
+                                             'client_id': settings.OAUTH_CLIENT_ID,  # your clinet id
+                                             'client_secret': settings.OAUTH_CLIENT_SECRET,  # your client secret
+                                             'username': user_details[0].username,  # your username that you get from user
+                                             'password': password,  # your password that you get from user
+                                             'redirect_uri': request.build_absolute_uri('/menu')
+                                             }
+                                  )
+                print(r.status_code)
+                return Response(r.json())
+            else:
+                return Response(json.loads('{"message": "User does not exists."}'))
+        except Exception as e:
+            error = {"message": e.__str__()}
+            return Response(json.loads(json.dumps(error)))
+
 
 class UserRegister(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
-        data = request.data 
+        data = request.data
         data = data.dict()
         serializer = serializers.RegisterSerializer(data=data)
         if serializer.is_valid():
@@ -66,7 +62,7 @@ class UserRegister(APIView):
 
 
 class Check(APIView):
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
-        
         return Response(json.loads('{"message": "Authenticated !"}'), status=status.HTTP_200_OK)
